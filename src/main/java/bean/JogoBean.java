f7bbada3650e2cfe64ff203783daaf9bbe8a25dc
KauanCarolino <kauancarolino248@gmail.com>
2024-10-20 13:33:24 -0300

    configuracao e salvar prontos

---
+++ src/main/java/bean/JogoBean.java
@@ -0,0 +1,45 @@
+package bean;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import javax.faces.bean.*;
+
+import dao.JogoDAO;
+import entidade.Jogo;
+
+@ManagedBean
+@ViewScoped
+public class JogoBean {
+	private Jogo jogo = new Jogo();
+	private List<Jogo> jogos;
+	
+	public void salvar() {
+		jogo.setDataCadastro(new Date());
+		jogo.setNumeroSorteado(new Random().nextInt(10)+1);
+		JogoDAO.salvar(jogo);
+		jogo = new Jogo();
+	}
+	
+	public List<Jogo> getJogos(){
+		if (jogos == null){
+			jogos = JogoDAO.Listar();
+		}
+		return jogos;
+	}
+
+	public Jogo getJogo() {
+		return jogo;
+	}
+
+	public void setJogo(Jogo jogo) {
+		this.jogo = jogo;
+	}
+
+	public void setJogos(List<Jogo> jogos) {
+		this.jogos = jogos;
+	}
+	
+	
+}
