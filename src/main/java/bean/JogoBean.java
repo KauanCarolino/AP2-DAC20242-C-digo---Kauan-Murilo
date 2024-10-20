e91fb950400f5514b0cebd586dc1ebddfb72b5a8
KauanCarolino <kauancarolino248@gmail.com>
2024-10-20 18:30:08 -0300

    excluir e editar pronto

--- src/main/java/bean/JogoBean.java
+++ src/main/java/bean/JogoBean.java
@@ -12,34 +12,44 @@ import entidade.Jogo;
 @ManagedBean
 @ViewScoped
 public class JogoBean {
-	private Jogo jogo = new Jogo();
-	private List<Jogo> jogos;
-	
-	public void salvar() {
-		jogo.setDataCadastro(new Date());
-		jogo.setNumeroSorteado(new Random().nextInt(10)+1);
-		JogoDAO.salvar(jogo);
-		jogo = new Jogo();
-	}
-	
-	public List<Jogo> getJogos(){
-		if (jogos == null){
-			jogos = JogoDAO.Listar();
-		}
-		return jogos;
-	}
+    private Jogo jogo = new Jogo();
+    private List<Jogo> jogos;
 
-	public Jogo getJogo() {
-		return jogo;
-	}
+    public void salvar() {
+        jogo.setDataCadastro(new Date());
+        jogo.setNumeroSorteado(new Random().nextInt(10)+1);
+        JogoDAO.salvar(jogo);
+        jogo = new Jogo();
+    }
 
-	public void setJogo(Jogo jogo) {
-		this.jogo = jogo;
-	}
+    public List<Jogo> getJogos(){
+        if (jogos == null){
+            jogos = JogoDAO.Listar();
+        }
+        return jogos;
+    }
 
-	public void setJogos(List<Jogo> jogos) {
-		this.jogos = jogos;
-	}
-	
-	
+    public void excluir(Jogo jogo) {
+        JogoDAO.Excluir(jogo);
+        jogos = JogoDAO.Listar();
+    }
+
+    public void editar(Jogo jogo) {
+        System.out.println("Editing Jogo: " + jogo);
+        JogoDAO.editar(jogo);
+    }
+
+    public Jogo getJogo() {
+        return jogo;
+    }
+
+    public void setJogo(Jogo jogo) {
+        this.jogo = jogo;
+    }
+
+    public void setJogos(List<Jogo> jogos) {
+        this.jogos = jogos;
+    }
+
+
 }
